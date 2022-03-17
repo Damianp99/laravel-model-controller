1f8e5025e0aba622154249ff8e5417100df8ff0a
Damianp99 <damianperez200199@gmail.com>
2022-03-17 20:24:05 +0100

    includes of header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,13 @@
+<header class="container">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">MOVIES INDEX</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">LET'S SEE</a>
+        </li>
+      </ul>
+</header>
